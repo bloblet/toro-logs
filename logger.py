ba220ee579ba1bb2fcc8d1c5d5450aad52ef8894
enrval <evguidesrc@gmail.com>
2020-07-02 11:47:52 -0400

    added stub on_log function

--- logger.py
+++ logger.py
@@ -7,3 +7,8 @@ class Logger(commands.Cog):
     @commands.command(name='test')
     async def test(self, ctx):
         await log_channel.send('Your magic is working properly ^.^')
+
+    async def on_log(self, log):
+        #save the log to a json file
+        #log_channel.send(log)
+        pass 
